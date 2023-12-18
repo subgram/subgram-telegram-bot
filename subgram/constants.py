7849f7f430501b6c8ebc0a9b02e83e3ec4b0c2f0
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2023-12-18 19:47:25 +0000

    sdk is now a standalone package

--- subgram/constants.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class EventType(str, Enum):
-    SUBSCRIPTION_STARTED = "subscription.started"
-    SUBSCRIPTION_RENEWED = "subscription.renewed"
-    SUBSCRIPTION_RENEW_FAILED = "subscription.renew_failed"
-    SUBSCRIPTION_CANCELLED = "subscription.cancelled"
-    SUBSCRIPTION_UPGRADED = "subscription.upgraded"
-
